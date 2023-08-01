ec54185a304f5a60dd2624566bcd55205d2ef925
pfigueiredo <klisarkk@gmail.com>
2023-08-01 12:11:34 +0100

    fix: reset history pointer on submit

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import * as Terminal from "./components/Terminal";
 import * as ContextProvider from "./contexts";
 import { TerminalContextProvider as _TerminalContextProvider } from "./contexts/TerminalContext";
+import type { TerminalProps } from "./components/Terminal";
 
-export function ReactTerminal(props: any): any {
+export function ReactTerminal(props: TerminalProps) {
   return (
     <ContextProvider.default>
       <Terminal.default {...props} />
