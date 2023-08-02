eb5e7c082531abb85a25d6ebf19d0cc7f0768167
pfigueiredo <klisarkk@gmail.com>
2023-08-02 12:07:54 +0100

    feat: add copy/paste on right click

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,14 +4,35 @@ import { StyleContext } from "../contexts/StyleContext";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { useTerminal } from "../contexts/TerminalContext";
 import { useCurrentLine, useScrollToBottom } from "../hooks/editor";
+import { MouseEventHandler } from "react";
 
 export default function Editor(props: any) {
-  const wrapperRef = React.useRef(null);
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
   const style = React.useContext(StyleContext);
   const themeStyles = React.useContext(ThemeContext);
-  const { store } = useTerminal();
+  const { store, send } = useTerminal();
 
   useScrollToBottom(store.bufferedContent, wrapperRef);
+
+  // adds the paste/copy actions to the context menu
+  const onMouseDown: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (!wrapperRef.current) { return; }
+
+    if (e.button === 2) {
+      wrapperRef.current.contentEditable = "true";
+
+      // wait just enough for 'contextmenu' to fire
+      setTimeout(() => {
+        wrapperRef.current.contentEditable = "false"
+      }, 20);
+    }
+
+  };
+
+  const onpaste: React.ClipboardEventHandler = e => {
+    e.preventDefault();
+    send({ type: "PASTE", text: e.clipboardData.getData("text") })
+  };
 
   const {
     enableInput,
@@ -40,6 +61,8 @@ export default function Editor(props: any) {
       id="terminalEditor"
       tabIndex={0}
       ref={wrapperRef}
+      onMouseDown={onMouseDown}
+      onPaste={onpaste}
       className={`${style.editor} ${!showControlBar ? style.curvedTop : null} ${showControlBar ? style.editorWithTopBar : null
         }`}
       style={{ background: themeStyles.themeBGColor }}
